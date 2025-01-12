799a1e8d9c0ead41117379e7492b06ba3618598c
Pradesh Mainali <pradeshm10@gmail.com>
2025-01-11 19:16:04 -0500

    fixed layout

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,7 +4,6 @@ import App from "./components/App";
 import NotFound from "./components/pages/NotFound";
 import HomePage from "./components/pages/Homepage"; 
 import ProfilePage from "./components/pages/ProfilePage"
-import Layout from "./components/pages/layout";
 
 import {
   createBrowserRouter,
@@ -14,21 +13,17 @@ import {
 } from 'react-router-dom'
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import Layout from "./components/pages/layout";
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
 const GOOGLE_CLIENT_ID = "465324171584-jgurca8sfthunf91v7q4agppmuoir1d0.apps.googleusercontent.com";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route errorElement={<NotFound />} element={<App />}>
-      <Route element={<Layout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/profile" element={<ProfilePage />} />
       </Route>
-    </Route>
   )
 );
-
 
 // renders React Component "Root" into the DOM element with ID "root"
 ReactDOM.createRoot(document.getElementById("root")).render(
