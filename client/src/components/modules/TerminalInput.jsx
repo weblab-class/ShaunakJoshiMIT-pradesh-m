52d5e3e06b890404e5a44dafcdabbb2c59002723
Shaunak Joshi <shaunak.a.joshi@gmail.com>
2025-01-13 00:05:47 -0500

    auto-focus user on textbox in terminal

--- client/src/components/modules/TerminalInput.jsx
+++ client/src/components/modules/TerminalInput.jsx
@@ -1,7 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useRef } from "react";
+import { useLocation } from "react-router-dom";
+
 
 const TerminalInput = (props) => {
+
+    const inputRef = useRef(null);
+    const location = useLocation()
     const [input, setInput] = useState("");
+
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        };
+    }, [location.pathname])
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -14,6 +26,7 @@ const TerminalInput = (props) => {
         <form className = "terminal-input" onSubmit = {handleSubmit}>
             <span className="CurrentUser"> {`<${props.username}>`}</span>
                 <input
+                        ref = {inputRef}
                         type = "text"
                         value = {input}
                         onChange = {(e) => setInput(e.target.value)}
