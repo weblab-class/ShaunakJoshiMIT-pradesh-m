46a3d6d9223c53c4529d210f74e2f22b0faf3f9e
Pradesh Mainali <pradeshm10@gmail.com>
2025-01-12 19:16:59 -0500

    Now deleted references to terminal outside of layout

--- client/src/components/pages/Homepage.jsx
+++ client/src/components/pages/Homepage.jsx
@@ -4,7 +4,6 @@ import "../../utilities.css";
 import { UserContext } from "../App.jsx";
 import "../styles/HomePage.css"; 
 import Layout from '../layout.jsx';
-import Terminal from "../modules/terminal.jsx";
 
 
 const HomePage = () => {
