3e4a9a48c194cb22e3004ee45eec68e2a3c530ab
Pradesh Mainali <pradeshm10@gmail.com>
2025-01-12 19:20:27 -0500

    Preventing scrolling

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import NavBar from './modules/NavBar.jsx';
 import Terminal from './modules/terminal.jsx';
+
 const Layout = ({ children, currentPage }) => {
     return (
-        <div>
+        <div style={{ overflow: 'hidden', height: '100vh' }}>
             <NavBar currentPage={currentPage} />
-            <main>{children}</main>
-            <Terminal username = "shaunakj"/>
+            <main style={{ overflow: 'hidden' }}>{children}</main>
+            <Terminal username="shaunakj" />
         </div>
     );
 };
