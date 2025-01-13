bbea031d0433496e46c46c6a38967c8e431f5644
Shaunak Joshi <shaunak.a.joshi@gmail.com>
2025-01-12 19:24:45 -0500

    terminal changes

---
+++ client/src/components/modules/TerminalContext.jsx
@@ -0,0 +1,12 @@
+import react, { useState, useContext, createContext } from "react";
+
+
+const TerminalContext = createContext();
+
+const UserContext = () => {
+    const[history, setHistory] = useState([]);
+
+    const addHistory = ()
+
+
+}
