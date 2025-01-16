b1e164c5e5bcb4af1c1e2c7c7d426e7abbe1eb1b
Shaunak Joshi <shaunak.a.joshi@gmail.com>
2025-01-16 17:39:58 -0500

    merge conflicts again

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,14 +6,6 @@ import { socket } from "../client-socket";
 import { get, post } from "../utilities";
 export const UserContext = createContext(null);
 
-<<<<<<< HEAD
-
-
-/**
- * Define the "App" component
- */
-=======
->>>>>>> 8ee3a1dd50e20a4ef28c8df2a07e15fc746b2165
 const App = () => {
   const [userId, setUserId] = useState(undefined);
   const [decoded, setDecoded] = useState(() => {
@@ -34,40 +26,6 @@ const App = () => {
   const handleLogin = (credentialResponse) => {
     const userToken = credentialResponse.credential;
     const decodedCredential = jwt_decode(userToken);
-<<<<<<< HEAD
-
-    localStorage.setItem("decodedToken", JSON.stringify(decodedCredential));
-
-
-    console.log(`Logged in as ${decodedCredential.name}`);
-
-    post("/api/login", { token: userToken })
-    .then((user) => {
-      console.log("Server responded with user:", user._id);
-      setUserId(user._id);
-
-      get("/api/user", { userid: user._id }).then((userObj) => {
-        console.log(userObj, " testing userobj")
-        if (userObj === null) {
-          const body = {
-            name: decodedCredential.name,
-            username: decodedCredential.name,
-            googleid: user._id,
-            friends: []
-          }
-          post("/api/addUser", body).then((userObj) => {
-            console.log(`Added user ${userObj.name}`)
-          })
-        }
-      })
-
-      post("/api/initsocket", { socketid: socket.id });
-      navigate("/home")
-    })
-    .catch((error) => {
-      console.error("Error during /api/login:", error);
-    });
-=======
     localStorage.setItem("decodedToken", JSON.stringify(decodedCredential));
     console.log(`Logged in as ${decodedCredential.name}`);
 
@@ -82,7 +40,6 @@ const App = () => {
       .catch((error) => {
         console.error("Error during /api/login:", error);
       });
->>>>>>> 8ee3a1dd50e20a4ef28c8df2a07e15fc746b2165
   };
 
   const handleLogout = () => {
