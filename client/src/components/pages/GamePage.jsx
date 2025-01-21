a4c82549720dca43af3cac04de3051702ae38f04
Pradesh Mainali <pradeshm10@gmail.com>
2025-01-21 17:56:37 -0500

    fixing imports

--- client/src/components/pages/GamePage.jsx
+++ client/src/components/pages/GamePage.jsx
@@ -1,6 +1,6 @@
 // GamePage.jsx
 import React, { useState, useEffect } from "react";
-import Terminal from "../modules/terminal";
+import Terminal from "../modules/Terminal";
 import MazeWrapper from "../modules/MazeWrapper";
 import { useParams, useNavigate } from "react-router-dom";
 import "../styles/GamePage.css";
@@ -8,8 +8,6 @@ import AppointmentModal from "../modules/AppointmentModal";
 import { io } from "socket.io-client"
 
 const socket = io("http://localhost:3000")
-
-
 
 const GamePage = (props) => {
     const { lobbyCode} = useParams();
