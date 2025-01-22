7a4a485d974c2bc83788d2b659e3b631536fa36b
Pradesh Mainali <pradeshm10@gmail.com>
2025-01-22 17:37:05 -0500

    deployment

--- client/src/components/modules/Maze.jsx
+++ client/src/components/modules/Maze.jsx
@@ -1,14 +1,12 @@
-// Maze.jsx
 import React, { useCallback, useState } from 'react';
 import ReactFlow, { addEdge, Background, Controls } from 'reactflow';
 import 'reactflow/dist/style.css';
-import FileNode from './FileNode'; // Your updated custom node with handles
+import FileNode from './FileNode'; 
 
 
 export default function Maze({ gameObj }) {
 
 
-  // Node Click Handler
   const onNodeClick = useCallback((event, node) => {
     if (!node?.data?.challenge) return;
     const { question, isImpossible } = node.data.challenge;
@@ -19,10 +17,7 @@ export default function Maze({ gameObj }) {
     );
   }, []);
 
-  // No need for onConnect since edges are static
-  // If you want to allow dynamic connections, you can implement onConnect
 
-  // Define node types
   const nodeTypes = { folderNode: FileNode };
 
   return (
@@ -45,9 +40,7 @@ export default function Maze({ gameObj }) {
         nodesConnectable={false}
         edgesFocusable={false}
       >
-        {/* Neon green grid background */}
         <Background color="#00ff00" gap={16} />
-        {/* Hide the controls since user cannot interact */}
         <Controls style={{ display: 'none' }} />
       </ReactFlow>
     </div>
